93773833885aea6788af09128a1bf736e0c66b09
kostaskoukos <kkonst2014@gmail.com>
2024-08-14 16:44:57 +0300

    implement downloading logic and error handling

--- main.go
+++ main.go
@@ -1,47 +1,50 @@
 package main
 
 import (
-	"io"
+	"bytes"
 	"net/http"
 	"os"
 
 	"github.com/kkdai/youtube/v2"
 )
 
-func DlSong(id string) {
-	videoID := "BaW_jenozKc"
+type Video struct {
+	Title  string
+	Buffer []byte
+}
+
+func DlSong(id string) (*Video, error) {
 	client := youtube.Client{}
 
-	video, err := client.GetVideo(videoID)
+	video, err := client.GetVideo(id)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	formats := video.Formats.WithAudioChannels() // only get videos with audio
+	formats := video.Formats.WithAudioChannels()
 	stream, _, err := client.GetStream(video, &formats[0])
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer stream.Close()
 
-	file, err := os.Create("video.mp3")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, stream)
-	if err != nil {
-		panic(err)
-	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return &Video{Title: video.Title, Buffer: buf.Bytes()}, nil
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		DlSong("")
-		w.Write([]byte("hi from golang"))
+	mux.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
+		vid, err := DlSong(r.PathValue("id"))
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		} else {
+			w.Header().Set("Content-Disposition", "attachment; filename="+vid.Title+".mp3")
+			w.Header().Set("Content-Type", "audio/mp3")
+			w.Write(vid.Buffer)
+		}
 	})
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), mux)
